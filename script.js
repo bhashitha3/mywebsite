71142ff39712f2c729f0b28de3f75080e6e70a3f
Bhashitha J <117940403+bhashitha3@users.noreply.github.com>
2023-06-27 15:30:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for anchor links
+document.addEventListener('DOMContentLoaded', function() {
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+    
+    for (let link of anchorLinks) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+          });
+        }
+      });
+    }
+  });
+  
